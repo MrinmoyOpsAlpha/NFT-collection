dde1d3ee17536ecde15a52d52e5aa7414f793eb6
Mrinmoy Singha <mrinmoysingha064@gmail.com>
2022-06-11 23:54:16 +0530

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xcEbdC901636a16ED83B56034Cd0d0690D254cB07";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = {WHITELIST_CONTRACT_ADDRESS,METADATA_URL};
